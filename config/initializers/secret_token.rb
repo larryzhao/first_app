c6741f7a5af186c67da749a15cd43fd047a72d09
U-SAP_ALL\I053892 <I053892@PVGN50793656A.(none)>
2011-06-25 22:10:53 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'aceb78d4849573d75ddcd98893789d88818ffccc71485d03f450bd0eb9f2a444fe728b4ca9b59f89634d2423f3abf189a395d1604c926ab4652a31496cf44a32'
